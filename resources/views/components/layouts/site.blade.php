33646397661583f2a0a0bc81603b3ce8913ea1b4
Franklin <habidasf@gmail.com>
2026-07-13 19:24:58 +0530

    UI updated

--- resources/views/components/layouts/site.blade.php
+++ resources/views/components/layouts/site.blade.php
@@ -22,6 +22,9 @@
         <meta name="keywords" content="{{ $metaKeywords }}">
     @endif
     <title>{{ isset($title) ? $title.' - '.$siteName : $siteName }}</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body style="--site-primary: {{ $primaryColor }}; --site-secondary: {{ $secondaryColor }};">
